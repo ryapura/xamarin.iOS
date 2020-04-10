0612017531c671f04f7f49317a649dfff595218d
Raul <ryapura@ext.gruposancorseguros.com>
2020-04-10 18:59:31 -0300

    basic controls for buttons

---
+++ basic.Controls/ViewController.cs
@@ -0,0 +1,70 @@
+﻿using Foundation;
+using System;
+using UIKit;
+
+namespace basic.Controls
+{
+    public partial class ViewController : UIViewController
+    {
+        public ViewController(IntPtr handle) : base(handle)
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            //eventos para el Boton 1
+            btn1.TouchUpInside += (sender, e) =>
+            {
+                //aqui creamos la alerta
+                var messageBox = UIAlertController.Create( "Titulo", "Click desde btn1" ,UIAlertControllerStyle.Alert);
+                //agregamos la accion
+                messageBox.AddAction(UIAlertAction.Create("OK", UIAlertActionStyle.Default, null));
+                //lo presentamos
+                PresentViewController(messageBox, true, null);
+
+            };
+
+            // para el boton 2
+            btn2.TouchUpInside += (sender, e) =>
+            {
+                var messageBox = UIAlertController.Create("Titulo 2 opciones", "msj con modal 2 acciones", UIAlertControllerStyle.Alert);
+                messageBox.AddAction(UIAlertAction.Create("Aceptar", UIAlertActionStyle.Default, alert => Console.WriteLine("Aceptar 2 opciones")));
+                messageBox.AddAction(UIAlertAction.Create("Cancelar", UIAlertActionStyle.Cancel, alert => Console.WriteLine("Cancelar 2 opciones")));
+
+                PresentViewController(messageBox, true, null);
+
+            };
+
+            // para el boton 3
+            btn3.TouchUpInside += Btn3_TouchUpInside;
+        }
+
+        private void Btn3_TouchUpInside(object sender, EventArgs e)
+        {
+            //Creamos nuestra Alerta
+            UIAlertController messageBox = UIAlertController.Create("Titulo Btn3", "Selecciones un item de abajo", UIAlertControllerStyle.ActionSheet);
+            //Agregamos las acciones
+            messageBox.AddAction(UIAlertAction.Create("Gano", UIAlertActionStyle.Default, (action) => Console.WriteLine("Item uno")));
+            messageBox.AddAction(UIAlertAction.Create("Empato", UIAlertActionStyle.Default, (action) => Console.WriteLine("Item dos")));
+            messageBox.AddAction(UIAlertAction.Create("Perdio", UIAlertActionStyle.Cancel, (action) => Console.WriteLine("Item tres")));
+
+            UIPopoverPresentationController presentationPopover = messageBox.PopoverPresentationController;
+            if (presentationPopover != null )
+            {
+                presentationPopover.SourceView = this.View;
+                presentationPopover.PermittedArrowDirections = UIPopoverArrowDirection.Up;
+
+            }
+            this.PresentViewController(messageBox, true, null);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
---
+++ basic.Controls/ViewController.designer.cs
@@ -0,0 +1,47 @@
+﻿// WARNING
+//
+// This file has been generated automatically by Visual Studio from the outlets and
+// actions declared in your storyboard file.
+// Manual changes to this file will not be maintained.
+//
+using Foundation;
+using System;
+using System.CodeDom.Compiler;
+using UIKit;
+
+namespace basic.Controls
+{
+    [Register ("ViewController")]
+    partial class ViewController
+    {
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btn1 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btn2 { get; set; }
+
+        [Outlet]
+        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UIButton btn3 { get; set; }
+
+        void ReleaseDesignerOutlets ()
+        {
+            if (btn1 != null) {
+                btn1.Dispose ();
+                btn1 = null;
+            }
+
+            if (btn2 != null) {
+                btn2.Dispose ();
+                btn2 = null;
+            }
+
+            if (btn3 != null) {
+                btn3.Dispose ();
+                btn3 = null;
+            }
+        }
+    }
+}
